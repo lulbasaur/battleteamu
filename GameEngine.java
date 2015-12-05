5f597eb90d22f7ac7cce091cf5022e7bbaf1d9a0
lulbasaur <lukas.hamberg@gmail.com>
2015-12-05 21:15:27 +0100

    added file

---
+++ GameEngine.java
@@ -0,0 +1,18 @@
+class GameEngine{
+
+
+    CoordinateGrid CD;
+    Coordinate p1Position;
+    Coordinate p2Position;
+
+    
+    Coordinate[] updateVector;
+    
+    public GameEngine(CoordinateGrid CD){
+	this.CD = CD;	
+    }
+
+    
+
+    
+}
