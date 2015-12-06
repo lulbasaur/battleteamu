89d0d3ab50d7a54b200b648575806252bb0ce05c
Ishak14 <andy.ishak10@gmail.com>
2015-12-06 18:39:52 +0100

    Lagt till killAlien
    kollar om det finns en alien i Yaxeln och gör den till en empty.

--- GameEngine.java
+++ GameEngine.java
@@ -16,10 +16,10 @@ class GameEngine{
 
     public void init(int x1,int y1,int x2,int y2, int alienNr){
 	p1Position.ship1();
-	p1Position.alterCoordinate(x1,y1);
+	p1Position.alterCoordinate(x1,y2); //startplats 7, 18
 
 	p1Position.ship2();
-	p2Position.alterCoordinate(x2,y2);
+	p2Position.alterCoordinate(x2,y2); //startplats  12,19
 	
 	aliens = new Coordinate[alienNr];
 	updateVector = new Coordinate[alienNr + 2];
@@ -31,17 +31,17 @@ class GameEngine{
 	    case 0:
 		//p1 right
 		if(p1Position.getX() < CD.dimensions && p1Position.getX() > 1){
-		    int updatedX = p1Position.getX() - 1;
+		    int updatedX = p1Position.getX() + 1;
 		    int sameY = p1Position.getY();
 		    p1Position.alterCoordinate(updatedX,sameY);
 		}
 		break;
 	    case 1:
 		//p1 left
-		if(p2Position.getX() < CD.dimensions && p2Position.getX() > 1){
-		    int updatedX = p2Position.getX() + 1;
-		    int sameY = p2Position.getY();
-		    p2Position.alterCoordinate(updatedX,sameY);
+		if(p1Position.getX() < CD.dimensions && p1Position.getX() > 1){
+		    int updatedX = p1Position.getX() - 1;
+		    int sameY = p1Position.getY();
+		    p1Position.alterCoordinate(updatedX,sameY);
 		}
 		break;
 	    }
@@ -51,7 +51,7 @@ class GameEngine{
 		//p2 right
 	    case 0:
 		if(p2Position.getX() < CD.dimensions && p2Position.getX() > 1){
-		    int updatedX = p2Position.getX() - 1;
+		    int updatedX = p2Position.getX() + 1;
 		    int sameY = p2Position.getY();
 		    p2Position.alterCoordinate(updatedX,sameY);
 		}
@@ -59,7 +59,7 @@ class GameEngine{
 		//p2 left
 	    case 1:
 		if(p2Position.getX() < CD.dimensions && p2Position.getX() > 1){
-		    int updatedX = p2Position.getX() + 1;
+		    int updatedX = p2Position.getX() - 1;
 		    int sameY = p2Position.getY();
 		    p2Position.alterCoordinate(updatedX,sameY);
 		}
@@ -74,6 +74,59 @@ class GameEngine{
 	return uV;
     }
     
+
+    public void killAlien(int player){
+	int frontAlien = -1;
+	if (player == 1){   //kolla om player 1 dödar en alien
+	    int xAxis = p1Position.getX();
+	    int maxLength = updateVector.length; 
+	    int i = 2;
+	   
+	    while(i < maxLength){
+		if (updateVector[i].isAlien() && updateVector[i].getX() == xAxis){
+		    if (updateVector[i].getY() > frontAlien){
+			frontAlien = i;
+		    }
+		}
+		i++;
+	    }
+	    updateVector[frontAlien].empty();
+	}		    	    
+	if (player == 2){  //kolla om player 2 dödar en alien
+	    int xAxis = p1Position.getX();
+	    // int yAxis = (CD.dimensions - 2);
+	    int maxLength = updateVector.length; /// kolla upp det
+	    int i = 2;
+	
+	    while(i < maxLength){
+		if (updateVector[i].isAlien() && updateVector[i].getX() == xAxis){
+		    if (updateVector[i].getY() > frontAlien){
+			frontAlien = i;
+		    }
+		}
+		i++;
+	    }
+	    updateVector[frontAlien].empty();
+	}	
+    }
+    
+    //   public void moveAlien(){
+	
+    
+
+    /*
+
+fire metod:
+
+int player
+
+om player 1 kolla vart han befinner sig i x led
+
+finns det några alien i x led
+
+
+
+     */
     
     
 }
