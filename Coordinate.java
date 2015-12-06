1448867fbc2e18ecc625d169f33c4ef32ae18c7e
lulbasaur <lukas.hamberg@gmail.com>
2015-12-06 13:56:55 +0100

    made change

--- Coordinate.java
+++ Coordinate.java
@@ -13,9 +13,13 @@ class Coordinate{
     public Coordinate(int x, int y){
 	this.x = x;
 	this.y = y;
-	this.empty = true;
     }
 
+    public void alterCoordinate(int x, int y){
+	this.x = x;
+	this.y = y;
+    }
+    
     public int getX(){ return x; }
     public int getY(){ return y; }
 
--- GameEngine.java
+++ GameEngine.java
@@ -4,6 +4,7 @@ class GameEngine{
     CoordinateGrid CD;
     Coordinate p1Position;
     Coordinate p2Position;
+    Coordinate[] aliens;
 
     
     Coordinate[] updateVector;
@@ -12,7 +13,56 @@ class GameEngine{
 	this.CD = CD;	
     }
 
-    
+    public void init(int x1,int y1,int x2,int y2){
+	p1Position.alterCoordinate(x1,y1);
+	p2Position.alterCoordinate(x2,y2);
+    }
 
+    public void movePlayer(int player, int rightleft){
+	if(player == 1){
+	    switch(rightleft){
+	    case 0:
+		//p1 right
+		if(p1Position.getX() < CD.dimensions && p1Position.getX() > 1){
+		    int updatedX = p1Position.getX() - 1;
+		    int sameY = p1Position.getY();
+		    p1Position.alterCoordinate(updatedX,sameY);
+		}
+		break;
+	    case 1:
+		//p1 left
+		if(p2Position.getX() < CD.dimensions && p2Position.getX() > 1){
+		    int updatedX = p2Position.getX() + 1;
+		    int sameY = p2Position.getY();
+		    p2Position.alterCoordinate(updatedX,sameY);
+		}
+		break;
+	    }
+	}
+	else if(player == 2){
+	    switch(rightleft){
+		//p2 right
+	    case 0:
+		if(p2Position.getX() < CD.dimensions && p2Position.getX() > 1){
+		    int updatedX = p2Position.getX() - 1;
+		    int sameY = p2Position.getY();
+		    p2Position.alterCoordinate(updatedX,sameY);
+		}
+		break;
+		//p2 left
+	    case 1:
+		if(p2Position.getX() < CD.dimensions && p2Position.getX() > 1){
+		    int updatedX = p2Position.getX() + 1;
+		    int sameY = p2Position.getY();
+		    p2Position.alterCoordinate(updatedX,sameY);
+		}
+		break;
+	    }
+	}
+	
+    }
+
+    
+    
     
 }
