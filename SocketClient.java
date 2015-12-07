f8655be747ebe5b91b840cbd1fa2094ebf765377
drante <anton.kallbom@gmail.com>
2015-12-07 01:29:23 +0100

    lagt in publik IP for test

--- SocketClient.java
+++ SocketClient.java
@@ -10,7 +10,7 @@ class SocketClient{
     public static void main(String[] args) {
 	ObjectInputStream inFromServer;
 	ObjectOutputStream outToServer;
-	String server = "localhost";
+	String server = "83.253.0.226";
 	int port = 2044;
 
 	SocketClientDriver SCD = new SocketClientDriver(server, port);
