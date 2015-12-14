e442585bd8adf5dfae5df7d9699f058399615f28
lulbasaur <lukas.hamberg@gmail.com>
2015-12-14 14:57:12 +0100

    asdasd

--- SocketClient.java
+++ SocketClient.java
@@ -13,7 +13,7 @@ class SocketClient{
 	//String server = "83.253.0.226";
 	String server = "130.238.251.55";
 	//String server =  "localhost";
-	int port = 2045;
+	int port = 2044;
 
 	SocketClientDriver SCD = new SocketClientDriver(server, port);
 
